393a4644ded86d1eec842240c1dd970a2fc2a54f
nursyah <nursyahjaya@gmail.com>
2023-09-03 21:19:44 +0800

    update

--- public/custom.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("keydown", function (event) {
-  // Check if the pressed key is the desired key
-  if (event.key === "Enter") {
-    console.log("Enter key pressed");
-    // Perform any desired action here
-  }
-});
--- publish.sh
+++
@@ -1,3 +0,0 @@
-# git add . && git commit -m "update" && git push && \
-rm dist/ -rf && pnpm build && \
-surge dist/ https://nursyah.surge.sh
